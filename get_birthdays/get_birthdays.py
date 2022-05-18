1e3eb2d8b4e7a72910e46667c8ada2079fb00068
Volodymyr Kravchenko <vlodya@gmail.com>
2022-05-18 14:03:54 +0300

    Update to 0.1.1

--- get_birthdays/get_birthdays.py
+++ get_birthdays/get_birthdays.py
@@ -6,15 +6,19 @@
 import datetime
 
 
-def get_birthdays_per_week(users: list, run_day=datetime.datetime.now().date()) -> None:
+def get_birthdays_per_week(users: list, output_mode: str, run_day=datetime.datetime.now().date()) -> None:
     """Функція get_birthdays_per_week(colleagues: list, run_day: datetime) -> None виводить список Днів Народження в
-    найближчі 7 днів. В понеділок та неділю виведення починається з минулої суботи, в інші дні - з поточного дня.
+    найближчі 7 днів.
     :param users: список словників users, кожен словник в якому обов'язково має ключі name: str - ім'я
     та birthday: str - дата формату str 'DD.MM.YYYY'
+    :param output_mode: форма виведення:
+         0 - виведення починається з поточного дня,
+         1 - виведення починається з наступного понеділка (в понеділок - з поточного дня),
+         2 - виведення починається з поточного дня (в понеділок та неділю - з минулої суботи)
     :param run_day: дата, на яку потрібно вивести результат. По замовчуванню - поточна дата.
     :return: None
     """
-    date_dict = empty_dates(run_day)
+    date_dict = empty_dates(run_day, output_mode)
     for user in users:
         bday = datetime.datetime.strptime(user['birthday'], '%d.%m.%Y').date()
         for key, value in date_dict.items():
@@ -22,21 +26,32 @@ def get_birthdays_per_week(users: list, run_day=datetime.datetime.now().date()) 
                 value.append(user['name'])
 
     print('Happy birthday next week!')
+    week_end = []
     for k, v in date_dict.items():
+        if k.weekday() in (5, 6) and output_mode in ('0', '1'):
+            week_end.extend(v)
+            continue
+        if k.weekday() == 0:
+            v.extend(week_end)
         print(f"{datetime.datetime.strftime(k, '%A')}: {', '.join(v) if v else '-'}")
 
 
-def empty_dates(run_day: datetime) -> dict:
+def empty_dates(run_day: datetime, output_mode: str) -> dict:
     """Допоміжна функція empty_dates(run_day: datetime) -> dict створює словник, ключами якого є найближчі 7 днів, а
-    значеннями - порожні списки. В понеділок та неділю словник починається з минулої суботи, в інші дні - з поточного
-    дня run_day.
+    значеннями - порожні списки.
     :param run_day: дата запуску програми
+    :param output_mode: форма виведення:
+         0 - виведення починається з поточного дня,
+         1 - виведення починається з наступного понеділка (в понеділок - з поточного дня),
+         2 - виведення починається з поточного дня (в понеділок та неділю - з минулої суботи)
     :return: словник для обробки основною процедурою.
     """
     if run_day.weekday() == 0:
         begin_day = -2
     elif run_day.weekday() == 6:
         begin_day = -1
+    elif run_day.weekday() > 0 and output_mode == '1':
+        begin_day = 5 - run_day.weekday()
     else:
         begin_day = 0
     date_dict = {run_day + datetime.timedelta(days=i): [] for i in range(begin_day, begin_day + 7)}
--- get_birthdays/main.py
+++ get_birthdays/main.py
@@ -2,19 +2,31 @@ from pathlib import Path
 import csv
 import sys
 from get_birthdays.get_birthdays import get_birthdays_per_week
+# from get_birthdays import get_birthdays_per_week
 
 
-def start():
+if __name__ == '__main__':
+    # def start():
+    output_mode = '0'
     if len(sys.argv) < 2:
         print('''
         Ви не зазначили обов'язковий параметр Source. Формат запуску:
-        reminder Source.csv
-        де Source.csv - файл з даними колег 
+        reminder Source.csv [output_mode=0]
+        де Source.csv - файл з даними колег,
+        output_mode - форма виведення:
+         0 - виведення починається з поточного дня,
+         1 - виведення починається з наступного понеділка (в понеділок - з поточного дня),
+         2 - виведення починається з поточного дня (в понеділок та неділю - з минулої суботи)
         ''')
         exit(0)
+
+    if len(sys.argv) > 2 and sys.argv[2] in ('1', '2'):
+        output_mode =sys.argv[2]
+
+    # print(output_mode)
 
     if sys.argv[1]:
         data_file = Path(sys.argv[1])
         with open(data_file, encoding='utf-8-sig') as csvfile:
             users = list(csv.DictReader(csvfile, delimiter=';'))
-        get_birthdays_per_week(users)
+        get_birthdays_per_week(users, output_mode)
